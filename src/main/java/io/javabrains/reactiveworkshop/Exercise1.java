b280f309728d9a75f6b2be4240a6c238d2c3933e
AmitJ95 <49363678+AmitJ95@users.noreply.github.com>
2025-02-27 14:10:13 +0530

    updated

--- src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
@@ -8,12 +8,24 @@ public class Exercise1 {
 
         // Print all numbers in the intNumbersStream stream
         // TODO: Write code here
+    	
+    	StreamSources.intNumbersStream().
+    	forEach(e -> System.out.println(e));
+    	
 
         // Print numbers from intNumbersStream that are less than 5
         // TODO: Write code here
+    	 System.out.println("Numbers less than 5 are: ");
+    	StreamSources.intNumbersStream().filter(i -> i<5)
+    	.forEach(e -> System.out.println(e));
+    	
 
         // Print the second and third numbers in intNumbersStream that's greater than 5
         // TODO: Write code here
+    	
+    	System.out.println("2nd and 3rd Numbers greater than 5 are: ");
+    	//StreamSources.intNumbersStream().filter(i -> i>5)
+    	
 
         //  Print the first number in intNumbersStream that's greater than 5.
         //  If nothing is found, print -1
